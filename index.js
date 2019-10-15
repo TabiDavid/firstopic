45cd34eb919ca75e5e3e322a026e96bd6c45d3f9
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-10-14 21:03:27 -0500

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function areaPerimeter(){
+
+    var length = parseFloat(document.getElementById("Write number 1").value);
+    var width = parseFloat(document.getElementById("write number 2").value);
+var operator = (2 * length) + (2 * width);
+var perimeter = Operator;
+
+document.getElementById("outPut").innerHTML = perimeter;
+}
