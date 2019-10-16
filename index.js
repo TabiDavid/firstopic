04057c1e2c46043ef560077eb6af20ed108d0b2e
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-10-15 22:08:09 -0500

    commit

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ function areaPerimeter(){
     var length = parseFloat(document.getElementById("write number 1").value);
     var width = parseFloat(document.getElementById("write number 2").value);
 var operator = (2 * length) + (2 * width);
-var perimeter = operator;
+
+var perimeter = result(operator);
 
 document.getElementById("outPut").innerHTML = perimeter;
 
 }
+
+function result(a) {
+var text = " ";
+    for (var i = 0; i >= a; i++){
+text += i; 
+return text;
+    
+
+    }
+}
+    /*if (a >= 500 & a <= 900){
+        
+        return "pay 10 % discount";
+
+    }
+  else if (a >= 901){
+
+    return "pay"
+
+  }  
+  else {
+      return "pay the price"
+  }
+}*/
